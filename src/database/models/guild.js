dc8357bcd447ca4ee9f4af252feba586580399f1
shotxd506 <shotxd506@gmail.com>
2025-08-03 13:43:59 +0000

    new start

--- src/database/models/guild.js
+++ src/database/models/guild.js
@@ -6,40 +6,6 @@ const guildSchema = new Schema({
         required: true,
         unique: true,
     },
-    welcome: {
-        channelId: {
-            type: String,
-            default: null,
-        },
-        roleId: {
-            type: String,
-            default: null,
-        },
-        enabled: {
-            type: Boolean,
-            default: false,
-        },
-        message: {
-            type: String,
-            default: 'Welcome {user} to {server}!',
-        },
-        background: {
-            type: String,
-            default: 'https://i.imgur.com/P9Fnt9y.jpeg',
-        },
-        embed: {
-            type: Boolean,
-            default: false,
-        },
-        mention: {
-            type: Boolean,
-            default: true,
-        },
-        color: {
-            type: String,
-            default: '#FFFFFF',
-        }
-    },
 });
 
 module.exports = model('Guild', guildSchema);
